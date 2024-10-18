87f38cfdbe228886ce536aac74a1c2969752d8b4
BogdanLzv <bogdan.lazov@gmail.com>
2024-10-18 14:01:47 +0200

    Add animal kind text color change on click

--- JavaScript/main page.js
+++ JavaScript/main page.js
@@ -137,19 +137,27 @@ tableRowMouseUp(logOutTableRow, logOutText);
 const animalKind = document.getElementById("animalKind-select");
 const animalKindList = document.getElementById("animalKind-list");
 const animalKindArrow = document.getElementById("animalKindArrow");
+const animalKindText = document.getElementById("animalKind-text");
 
 // Functions for changing detail lists display ˘˘˘˘˘˘
 
-const detailListDisplay = (eventTarget, changedTarget_1, changedTarget_2) => {
+const detailListDisplay = (eventTarget, changedTarget_1, changedTarget_2, changedTarget_3) => {
     eventTarget.addEventListener("click", () => {
         const targetDisplay = window.getComputedStyle(changedTarget_1).display;
         const targetTransform = window.getComputedStyle(changedTarget_2).transform;
-        if (targetDisplay === "none" && targetTransform === "none") {
+        const targetColor = window.getComputedStyle(changedTarget_3).color;
+
+        // Handle mismatch in color value
+        const isColorDarkMagenta = (targetColor === "rgb(139, 0, 139)"); // darkmagenta in RGB
+
+        if (targetDisplay === "none" && targetTransform === "none" && isColorDarkMagenta) {
             changedTarget_1.style.display = "flex";
             changedTarget_2.style.transform = "rotate(180deg)";
+            changedTarget_3.style.color = "rgb(187, 143, 169)"; // purple-grayish color
         } else {
             changedTarget_1.style.display = "none";
             changedTarget_2.style.transform = "none";
+            changedTarget_3.style.color = "darkmagenta";
         };
     });
 };
@@ -159,4 +167,4 @@ const detailListDisplay = (eventTarget, changedTarget_1, changedTarget_2) => {
 
 // Animal kind ˘˘˘˘˘˘
 
-detailListDisplay(animalKind, animalKindList, animalKindArrow);
+detailListDisplay(animalKind, animalKindList, animalKindArrow, animalKindText);
