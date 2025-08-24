b9c00dc18ada4b5344ffca297c80d5c07db91dc5
BogdanLzv <bogdan.lazov@gmail.com>
2025-08-24 16:36:33 +0200

    Test for login page

--- PHP/LoginPage.php
+++ PHP/LoginPage.php
@@ -1,2 +1,8 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = htmlspecialchars($_POST["username"]);
+    $password = htmlspecialchars($_POST["password"]);
+    $rememberMe = htmlspecialchars($_POST["rememberMe"]);
+}
